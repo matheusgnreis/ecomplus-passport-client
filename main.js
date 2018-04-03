5aa185e7f2d075cfafdaa727ceecdc3a32dc3621
Leonardo Matos <leomp120894@gmail.com>
2018-04-03 19:31:09 -0300

    ajax request, store customer session

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ window.EcomPassport = (function () {
 
   var storeId, lang, reqId
   var baseUri = 'https://passport.e-com.plus/v1/'
+  // customer session
+  // public profile and authentication
+  var session = {}
 
-  function makeId () {
+  var makeId = function () {
     // generate random 32 bytes string
     reqId = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -13,11 +16,61 @@ window.EcomPassport = (function () {
     }
   }
 
+  var ajaxRequest = function (method, endpoint, body, authenticate, successCallback) {
+    // call with AJAX
+    var ajax
+    if (window.XDomainRequest) {
+      // IE 8,9
+      ajax = new window.XDomainRequest()
+      ajax.onload = function () {
+        // treat response
+        successCallback(this)
+      }
+    } else {
+      // supported by recent browsers
+      ajax = new XMLHttpRequest()
+      ajax.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          // treat response
+          successCallback(this)
+        }
+      }
+    }
+
+    var url = baseUri + storeId + '/' + reqId + endpoint
+    ajax.open(method, url, true)
+    if (authenticate && typeof session.auth === 'object' && session.auth !== null) {
+      // add authentication headers
+      ajax.setRequestHeader('X-My-ID', session.auth.id)
+      ajax.setRequestHeader('X-Access-Token', session.auth.token)
+    }
+    if (body) {
+      // send JSON body
+      ajax.send(JSON.stringify(body))
+    } else {
+      ajax.send()
+    }
+  }
+
+  var sessionResponse = function (res) {
+    // set new session
+    var body = res.responseJSON
+    if (typeof body === 'object' && body !== null) {
+      // set global
+      session = body
+    }
+  }
+
   return {
     'init': function (StoreId, Lang) {
       // set store ID and language
       storeId = StoreId
       lang = Lang
+    },
+
+    // return current customer session
+    'session': function () {
+      return session
     },
 
     'loginPopup': function () {
@@ -49,6 +102,11 @@ window.EcomPassport = (function () {
           getCustomerInfo(false)
         }
       }, 400)
+    },
+
+    'getSession': function () {
+      // get customer info and authentication session
+      ajaxRequest('GET', '/token.json', null, false, sessionResponse)
     }
   }
 }())
