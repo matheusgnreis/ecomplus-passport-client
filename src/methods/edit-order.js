fe09e14fa8c96730000a2047c32301d87e619666
Talisson Ferreira <talissonf@gmail.com>
2019-08-22 13:42:00 -0300

    feat(methods): edit order

---
+++ src/methods/edit-order.js
@@ -0,0 +1,19 @@
+export default (self, body) => {
+  if (self.getAuthLevel <= 2) {
+    throw new Error('Unauthorized, need permission')
+  }
+
+  // Authenticated request
+  const authenticationId = self.session.auth.token.my_id
+  const accessToken = self.session.auth.token.my_id
+
+  return self.ecomClient
+
+    .passport({
+      url: `api/orders.json`,
+      authenticationId,
+      accessToken,
+      method: 'POST',
+      data: body
+    })
+}
