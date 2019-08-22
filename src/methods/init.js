dafbeab98d513a01eb9a1c24b461b296d027457d
Talisson Ferreira <talissonf@gmail.com>
2019-08-22 13:45:42 -0300

    feat(init): try to get cookie from session

---
+++ src/methods/init.js
@@ -0,0 +1,28 @@
+export default (self, document) => {
+  // try to get session from cookie
+  const decodedCookie = decodeURIComponent(document.cookie)
+  const ca = decodedCookie.split(';')
+
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(self.cookieName) === 0) {
+      // found cookie
+      const sessionJson = c.substring(self.cookieName.length, c.length)
+      try {
+        self.session = JSON.parse(sessionJson)
+        if (typeof self.session === 'object' && self.session !== null && !Array.isArray(self.session)) {
+          // ok
+          return
+        }
+      } catch (e) {
+        // invalid cookie value
+      }
+      // reset
+      self.session = {}
+      return
+    }
+  }
+}
