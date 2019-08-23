f47705a97e78012ad7476f6924b9cde3ae229fd4
Leonardo Matos <leomp120894@gmail.com>
2019-08-22 21:29:28 -0300

    refactor(consturctor): renaming methods, removing unecessary members
    BREAKING CHANGE: methods renamed, old methods will no more work

--- src/index.js
+++ src/index.js
@@ -1,77 +1,76 @@
+/**
+ * https://github.com/ecomclub/ecomplus-passport-client
+ * @author E-Com Club <ti@e-com.club>
+ * @license MIT
+ */
+
 import { _config } from '@ecomplus/utils'
-
 import emitter from './lib/emitter'
-import init from './lib/init'
 
-import makeId from './methods/make-id'
+import initSession from './methods/init-session'
 import setSession from './methods/set-session'
-import getCustomerName from './methods/customer-name'
-import getCustomer from './methods/customer-object'
-import getSession from './methods/session'
-import getOAuthSession from './methods/get-session'
-import getOAuthProviders from './methods/oauth-providers'
-import editCustomers from './methods/edit-customer'
-import editOrders from './methods/edit-order'
-import listOrders from './methods/list-orders'
-import viewOrder from './methods/view-order'
+import getCustomerName from './methods/get-customer-name'
+import getCustomer from './methods/get-customer-object'
+import fetchOauthProfile from './methods/fetch-oauth-profile'
+import fetchOAuthProviders from './methods/fetch-oauth-providers'
+import fetchLogin from './methods/fetch-login'
+import fetchOrdersList from './methods/fetch-orders-list'
+import fetchOrder from './methods/fetch-order'
 import isLogged from './methods/is-logged'
+import isAuthorized from './methods/is-authorized'
+import isVerified from './methods/is-verified'
 import logout from './methods/logout'
-import loginPopup from './methods/login-popup'
-import identify from './methods/login-rest'
+import popupOauthLink from './methods/popup-oauth-link'
+import popupLogin from './methods/popup-login'
+import requestRestApi from './methods/request-rest-api'
 
-export default function (
-  storeId = _config.get('store_id'),
-  document = typeof window === 'object' && window.document,
-  lang = _config.get('lang'),
-  storageKey = 'ecomPassport',
-  localStorage = typeof window === 'object' && window.localStorage,
-  cookieName = '_ecom_passport'
-) {
-  this.document = document
+const _store = _config.get('store_id')
+const _lang = _config.get('lang')
+const _document = typeof window === 'object' && window.document
+const _cookie = '_ecom_passport'
+
+export default function (storeId = _store, lang = _lang, document = _document, cookieName = _cookie) {
   this.storeId = storeId
   this.lang = lang
-  this.reqId = null
+  this.sessionId = ''
   this.session = {}
-  this.authLevel = 0
-  this.baseUri = 'https://passport.e-com.plus/v1/'
   this.cookieName = cookieName
+  this.document = document
 
   // this
   const self = this
 
   // setters
-  this.makeId = () => makeId(self)
-  this.setSession = data => setSession(self, localStorage, storageKey, data)
+  this.initSession = () => initSession(self)
+  this.setSession = data => setSession(self, data)
+  this.logout = () => logout(self, document)
 
   // getters
-  this.getAuthLevel = () => self.authLevel
   this.getCustomerName = () => getCustomerName(self)
   this.getCustomer = () => getCustomer(self)
-  this.getSession = () => getSession(self)
-  this.getOAuthSession = (callback) => getOAuthSession(self, callback)
-  this.getOAuthProviders = () => getOAuthProviders(self)
-
-  // methods
   this.isLogged = () => isLogged(self)
-  this.logout = () => logout(self, document)
-  this.loginPopup = enableSkip => loginPopup(self, enableSkip)
-  this.identify = (email, docNumber) => identify(self, email, docNumber)
-  this.editCustomers = (body) => editCustomers(self, body)
-  this.editOrders = (body) => editOrders(self, body)
-  this.viewOrder = (orderId) => viewOrder(self, orderId)
-  this.listOrders = (from, size) => listOrders(self, from, size)
+  this.isAuthorized = () => isAuthorized(self)
+  this.isVerified = () => isVerified(self)
 
-  // emitter
-  ;[
-    'on',
-    'off',
-    'once'
-  ].forEach(method => {
+  // rest api requests
+  this.fetchLogin = (email, docNumber) => fetchLogin(self, email, docNumber)
+  this.fetchOauthProfile = () => fetchOauthProfile(self)
+  this.fetchOAuthProviders = () => fetchOAuthProviders(self)
+  this.fetchOrdersList = (from, size) => fetchOrdersList(self, from, size)
+  this.fetchOrder = orderId => fetchOrder(self, orderId)
+  this.requestRestApi = (url, method, data) => requestRestApi(self, url, method, data)
+
+  // async open browser popup
+  this.popupOauthLink = url => popupOauthLink(self, url)
+  this.popupLogin = (enableSkip, baseUri) => popupLogin(self, enableSkip, baseUri)
+
+  // events emitter
+  ;['on', 'off', 'once'].forEach(method => {
     self[method] = (ev, fn) => {
       emitter[method](ev, fn)
     }
   })
 
   // init
-  init(self)
+  initSession(self)
 }
