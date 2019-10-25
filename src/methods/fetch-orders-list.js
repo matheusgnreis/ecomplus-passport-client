3cac9f0f666c47bee84e8e11fd15633618c1ddc9
Talisson Ferreira <talissonf@gmail.com>
2019-10-25 16:29:24 -0300

    revert: using ecomClient

--- src/methods/fetch-orders-list.js
+++ src/methods/fetch-orders-list.js
@@ -1,10 +1,14 @@
+import { store } from '@ecomplus/client'
 export default (self, from = 0, size = 10) => {
   const { orders } = self.getCustomer()
   const results = []
   const promises = []
   for (let i = 0; i < orders.length && i < (from + size); i++) {
-    const promise = self.requestApi(`/orders/${orders[i]._id}.json`).then(({ data }) => results.push(data))
-    promises.push(promise)
+    promises.push(store({
+      url: `/orders/${orders[i]._id}.json`,
+      storeId: self.storeId
+    })
+      .then(({ data }) => results.push(data)))
   }
   return Promise.all(promises).then(() => results)
 }
