c548b4c729468d9d8766da3d96e253041cb7ed51
Leonardo Matos <leomp120894@gmail.com>
2019-10-25 17:20:48 -0300

    style(fetch-orders-list): minor indentation/comments fix

--- src/methods/fetch-orders-list.js
+++ src/methods/fetch-orders-list.js
@@ -1,9 +1,11 @@
 import { store } from '@ecomplus/client'
+
 export default (self, from = 0, size = 10) => {
   const { orders } = self.getCustomer()
   const results = []
   const promises = []
   for (let i = 0; i < orders.length && i < (from + size); i++) {
+    // use ecomClient.store to fetch cache (faster) API
     promises.push(store({
       url: `/orders/${orders[i]._id}.json`,
       storeId: self.storeId
