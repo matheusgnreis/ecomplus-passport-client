71e419b5ac2db0a285b77b25121cfd6210dc0522
Leonardo Matos <leomp120894@gmail.com>
2019-11-13 18:18:56 -0300

    fix(fetch-orders-list): must sort orders again due to async results push

--- src/methods/fetch-orders-list.js
+++ src/methods/fetch-orders-list.js
@@ -4,10 +4,12 @@ export default (self, from = 0, size = 10) => {
   const requestPromises = []
   const resultOrders = []
 
-  self.getCustomer().orders
-    // sort by order number desc
-    .sort((a, b) => b.number - a.number)
-    .slice(from, from + size)
+  // sort by order number desc
+  const sortOrdersFn = (a, b) => b.number - a.number
+
+  ;(self.getCustomer().orders || [])
+    // handle orders sorting and pagination first
+    .sort(sortOrdersFn).slice(from, from + size)
     .forEach(({ _id }) => {
       // use ecomClient.store to fetch cache (faster) API
       requestPromises.push(store({
@@ -17,5 +19,6 @@ export default (self, from = 0, size = 10) => {
         .then(({ data }) => resultOrders.push(data)))
     })
 
-  return Promise.all(requestPromises).then(() => resultOrders)
+  // must sort orders again due to async result pushes
+  return Promise.all(requestPromises).then(() => resultOrders.sort(sortOrdersFn))
 }
