a023bbf0504d673046ad6f2e8c3f70f7a4cc91ba
Leonardo Matos <leomp120894@gmail.com>
2019-11-13 20:55:14 -0300

    fix(set-customer): update cookie value with new customer

--- src/methods/set-customer.js
+++ src/methods/set-customer.js
@@ -1,12 +1,15 @@
+import setCookie from './../lib/set-cookie'
+
 export default (self, customer) => {
   if (typeof customer === 'object' && customer !== null && !Array.isArray(customer)) {
-    const { session } = self
+    const { document, cookieName, session } = self
     if (!session.customer) {
       session.customer = customer
     } else {
       // merge customer data
       Object.assign(session.customer, customer)
     }
+    setCookie(document, cookieName, JSON.stringify(session), 6)
   } else {
     throw new Error('Customer must be an object')
   }
