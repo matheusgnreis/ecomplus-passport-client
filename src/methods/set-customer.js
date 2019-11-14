b3c1a60fcbe80367c9162150b77667c3f0850479
Leonardo Matos <leomp120894@gmail.com>
2019-11-13 21:47:41 -0300

    fix(set-customer): limit customer fields to prevent excedent cookie size

--- src/methods/set-customer.js
+++ src/methods/set-customer.js
@@ -4,14 +4,39 @@ export default (self, customer) => {
   if (typeof customer === 'object' && customer !== null && !Array.isArray(customer)) {
     const { document, cookieName, session } = self
     if (!session.customer) {
-      session.customer = customer
-    } else {
-      // merge customer data
-      Object.assign(session.customer, customer)
+      session.customer = {}
     }
+
+    // merge customer data
+    // limit customer fields to prevent excedent cookie size
+    ;[
+      '_id',
+      'oauth_providers',
+      'locale',
+      'main_email',
+      'accepts_marketing',
+      'display_name',
+      'birth_date',
+      'registry_type',
+      'gender',
+      'favorites',
+      'currency_id',
+      'currency_symbol'
+    ].forEach(field => {
+      if (customer[field] !== undefined) {
+        session.customer[field] = customer[field]
+      }
+    })
+    if (customer.orders) {
+      // reduce order objects to ID and number only
+      session.customer.orders = customer.orders.map(({ _id, number }) => ({ _id, number }))
+    }
+
+    // update session cookie
     setCookie(document, cookieName, JSON.stringify(session), 6)
   } else {
     throw new Error('Customer must be an object')
   }
+
   return self
 }
