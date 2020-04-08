0380bcffbcd05b893c6394d39a8f3d27dffb901a
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-04-08 11:47:18 -0300

    fix: accessbility problem with title

--- src/lib/create-iframe.js
+++ src/lib/create-iframe.js
@@ -6,7 +6,7 @@ export default url => {
   // create hidden iframe and append to body
   const iframe = document.createElement('iframe')
   iframe.setAttribute('src', url)
-  iframe.setAttribute('name', 'E-Com Plus Passport for login and register')
+  iframe.setAttribute('title', 'E-Com Plus Passport for login and register')
   iframe.setAttribute('style', 'width:0;height:0;border:0;border:none;')
   document.body.appendChild(iframe)
 }
